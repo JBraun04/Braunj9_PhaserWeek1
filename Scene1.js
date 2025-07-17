93bbbbef9b44dc0962a2c3f3da8ab22e2ab8506c
Jack Braun <j.braun3456@gmail.com>
2025-07-17 13:22:13 -0400

    Created the images and wrote the code to make them move

--- Scene1.js
+++ Scene1.js
@@ -5,6 +5,9 @@ class Scene1 extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "assets/background.png");
+        this.load.image("ship", "assets/ship.png");
+        this.load.image("ship2", "assets/ship2.png");
+        this.load.image("ship3", "assets/ship3.png");
     }
 
     create() {
